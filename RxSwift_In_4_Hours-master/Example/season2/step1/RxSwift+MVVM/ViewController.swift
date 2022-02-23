dcd75c06eb39ddbe520fa74371f91ca8d3e28d97
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-24 02:07:58 +0900

    [Add] #1 - 곰튀김님 예제파일 추가
    곰튀김님 예제파일 추가

---
+++ RxSwift_In_4_Hours-master/Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  RxSwift+MVVM
+//
+//  Created by iamchiwon on 05/08/2019.
+//  Copyright © 2019 iamchiwon. All rights reserved.
+//
+
+import RxSwift
+import SwiftyJSON
+import UIKit
+
+let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
+
+class ViewController: UIViewController {
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var editView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+            self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
+        }
+    }
+
+    private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
+        guard let v = v else { return }
+        UIView.animate(withDuration: 0.3, animations: { [weak v] in
+            v?.isHidden = !s
+        }, completion: { [weak self] _ in
+            self?.view.layoutIfNeeded()
+        })
+    }
+
+    // MARK: SYNC
+
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+
+    @IBAction func onLoad() {
+        editView.text = ""
+        setVisibleWithAnimation(activityIndicator, true)
+
+        let url = URL(string: MEMBER_LIST_URL)!
+        let data = try! Data(contentsOf: url)
+        let json = String(data: data, encoding: .utf8)
+        self.editView.text = json
+        
+        self.setVisibleWithAnimation(self.activityIndicator, false)
+    }
+}
