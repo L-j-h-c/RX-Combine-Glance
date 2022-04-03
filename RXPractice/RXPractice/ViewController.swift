d103790b39c23118953f68a5b8f34c95c76df9ad
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-03 19:49:08 +0900

    [Feat] #1 - CH1 Observable
     CH1 Observable

--- RXPractice/RXPractice/ViewController.swift
+++ RXPractice/RXPractice/ViewController.swift
@@ -7,13 +7,84 @@
 
 import UIKit
 
+import RxSwift
+import SnapKit
+
 class ViewController: UIViewController {
+    
+    private let testLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "테스트 ㅋㅋ"
+        return lb
+    }()
+    
+    private lazy var testButton: UIButton = {
+        let bt = UIButton()
+        bt.setTitle("버튼이야", for: .normal)
+        bt.setTitleColor(.blue, for: .normal)
+        return bt
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
+        setLayout()
+        makeObservable()
+    }
+    
+    private func makeObservable() {
+        // Event를 받아옴 인자의 name은 자율로 변경 가능
+        let subscription1 = Observable.of(1,2,3).subscribe { t in
+            print(t)
+        }.dispose()
+        
+        // Event에 element를 받아서 어떠한 작업을 실행하는 클로저를 등록 가능.
+        // func subscribe(onNext: ((Self.Element) -> Void)? = nil, onError: ((Error) -> Void)? = nil, onCompleted: (() -> Void)? = nil, onDisposed: (() -> Void)? = nil) -> Disposable
+        let subscription2 = Observable.of(1,2,3).subscribe(onNext: { element in
+            print(element)
+        }).dispose()
+        
+        let subscription3 = Observable.of(1,2,3).subscribe(onNext: { t in
+            print(t)
+        }).dispose()
+        
+        // 아래의 방법이 Observer를 조금 더 세심하게 설정할 수 있는 거구나.
+        Observable<String>.create { observer in
+            // observer에 1이라는 element를 가진 Next이벤트를 보내는 것.
+            observer.onNext("1")
+            observer.onCompleted()
+            // 이러한 옵저버를 가지는 Disposable를 아래에서 리턴한다.
+            return Disposables.create()
+        }
+        .subscribe(
+            onNext: { print($0) }
+        )
+    }
+    
+    private func configureUI() {
+        testButton.addTarget(self, action: #selector(changeLabel), for: .touchUpInside)
+    }
+    
+    @objc
+    private func changeLabel() {
+        testLabel.text = "바꿈"
     }
 
+}
 
+extension ViewController {
+    private func setLayout() {
+        view.addSubview(testLabel)
+        view.addSubview(testButton)
+        
+        testLabel.snp.makeConstraints { make in
+            make.centerY.centerX.equalToSuperview()
+        }
+        testButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(testLabel.snp.bottom).offset(30)
+        }
+    }
 }
 
